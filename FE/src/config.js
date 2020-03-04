eb71acb1c451cec35d7de6a4e9648f787caac1d0
xavi Santamaria Viñas <xavisanta2@gmail.com>
2020-03-04 23:51:14 +0100

    Add config file

--- FE/src/config.js
+++ FE/src/config.js
@@ -5,11 +5,15 @@ var popup;
 var map;
 
 const Port = 9035;
-const apiURL = 'https://ecv-etic.upf.edu/node/';
-const PATH = `${apiURL}${Port}`;
-// const apiURL = 'http://127.0.0.1';
-// const PATH = `${apiURL}:${Port}`;
-
 var connection = null;
-var SocketURL = 'wss://ecv-etic.upf.edu/node/9035/ws/';
-// var SocketURL = 'ws://127.0.0.1:9035';
+
+// SERVER ECV:
+// const apiURL = 'https://ecv-etic.upf.edu/node/';
+// const PATH = `${apiURL}${Port}`;
+// var SocketURL = 'wss://ecv-etic.upf.edu/node/9035/ws/';
+
+// TEST LOCAL: 
+const apiURL = 'http://127.0.0.1';
+const PATH = `${apiURL}:${Port}`;
+var SocketURL = 'ws://127.0.0.1:9035';
+
