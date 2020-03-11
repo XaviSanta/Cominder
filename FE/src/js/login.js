bf3b72f6a1f123648a9001e38bb0f036bcc4bb9b
xavi Santamaria Viñas <xavisanta2@gmail.com>
2020-03-11 21:18:25 +0100

    Add login via google

--- FE/src/js/login.js
+++ FE/src/js/login.js
@@ -34,7 +34,7 @@ $('.loginForm-p').submit(function() {
   })
   .catch(err => {
     alert(err);
-  });;
+  });
   
   return false;
 });
@@ -97,5 +97,24 @@ function openLogin() {
 function logOut() {
   auth.signOut().then(() => {
     console.log('user signed out')
+  });
+}
+
+function loginWgoogle() {
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // ...
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
   });
 }
